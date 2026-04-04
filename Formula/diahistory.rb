d2151cc1d2a1c345b6f4953216364d9ee0fb86a1
aaron <ajwills@umich.edu>
2026-04-03 20:14:55 -0400

    Add diahistory formula

---
+++ Formula/diahistory.rb
@@ -0,0 +1,19 @@
+class Diahistory < Formula
+  desc "Capture and archive Dia browser conversations as local files"
+  homepage "https://github.com/aaronw122/diaHistory"
+  url "https://github.com/aaronw122/diaHistory/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "0de2d35bbd882ac1f8664edd85834265813695fd8208fd2d63de2ff84e819e34"
+  license "MIT"
+
+  depends_on macos: :sonoma
+
+  def install
+    system "swift", "build", "--disable-sandbox", "-c", "release"
+    bin.install ".build/release/diahistory"
+  end
+
+  test do
+    assert_match "Capture Dia browser chat conversations via the Accessibility API.",
+                 shell_output("#{bin}/diahistory --help")
+  end
+end
