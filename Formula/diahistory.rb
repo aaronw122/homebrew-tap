5db4916d39b05d693e3ff01d5fb4e8b7b306e507
Aaron Williams <aaron@Aarons-MacBook-Pro.local>
2026-04-15 17:46:46 -0400

    bump diahistory to v0.1.5

--- Formula/diahistory.rb
+++ Formula/diahistory.rb
@@ -1,8 +1,8 @@
 class Diahistory < Formula
   desc "Capture and archive Dia browser conversations as local files"
   homepage "https://github.com/aaronw122/diaHistory"
-  url "https://github.com/aaronw122/diaHistory/archive/refs/tags/v0.1.4.tar.gz"
-  sha256 "0a825b45019ce03e80a3b2f5a2220abf26e875c68d5550075091b265ab31ecfb"
+  url "https://github.com/aaronw122/diaHistory/archive/refs/tags/v0.1.5.tar.gz"
+  sha256 "49d3bb818efdde2d91b5d06a99163b258369a6c19a38ffeae491ff519804d623"
   license "MIT"
 
   depends_on macos: :sonoma
