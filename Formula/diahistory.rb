f3e1b904b0faf2c6b4af30d87247b01e86a59b32
aaron <ajwills@umich.edu>
2026-04-15 17:02:10 -0400

    diahistory 0.1.4: fix memory leak in long-running daemon

--- Formula/diahistory.rb
+++ Formula/diahistory.rb
@@ -1,8 +1,8 @@
 class Diahistory < Formula
   desc "Capture and archive Dia browser conversations as local files"
   homepage "https://github.com/aaronw122/diaHistory"
-  url "https://github.com/aaronw122/diaHistory/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "74aa7cc700e9fb8debac989c2317a9442f076b3b53656d544f73488ee7d9214a"
+  url "https://github.com/aaronw122/diaHistory/archive/refs/tags/v0.1.4.tar.gz"
+  sha256 "0a825b45019ce03e80a3b2f5a2220abf26e875c68d5550075091b265ab31ecfb"
   license "MIT"
 
   depends_on macos: :sonoma
