7a25f42fac72cffbb703b2abb21fd6bc79b5c07c
aaron <ajwills@umich.edu>
2026-04-18 15:07:26 -0400

    diahistory 0.1.9: fix TCC identity mismatch in permission flow

--- Formula/diahistory.rb
+++ Formula/diahistory.rb
@@ -1,8 +1,8 @@
 class Diahistory < Formula
   desc "Capture and archive Dia browser conversations as local files"
   homepage "https://github.com/aaronw122/diaHistory"
-  url "https://github.com/aaronw122/diaHistory/archive/refs/tags/v0.1.8.tar.gz"
-  sha256 "a937c23154d7426a62492e8739faf61952961d5731d37867cba4e9aafe4046a3"
+  url "https://github.com/aaronw122/diaHistory/archive/refs/tags/v0.1.9.tar.gz"
+  sha256 "3f332228adf3fcb548e1b9bb09edf235ee2c4c3495608a4a7b1e44dcf0bd3bd8"
   license "MIT"
 
   depends_on macos: :sonoma
